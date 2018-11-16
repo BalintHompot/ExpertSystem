257e381025b25104941a9f56789db98a736b7ee7
Balint Hompot <balinthompot@RN-145-90-138-33.eduroam.rug.nl>
2018-11-16 13:13:06 +0100

    blank electron\react project

---
+++ nutriexpert/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Nutrition Expert">
+        <header className="Nutrition Expert">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome to nutrition expert
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
